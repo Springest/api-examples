04ae89204d51d75f31c1ce3d8b827213f44836f4
Yorick Sijsling <yoricksijsling@gmail.com>
2012-02-22 15:22:57 +0100

    Update ruby/trainings.rb

--- ruby/trainings.rb
+++ ruby/trainings.rb
@@ -3,25 +3,25 @@ require 'open-uri'
 require 'nokogiri'
 
 def get_trainings(base, page_size = 10, offset = 0)
-    url = (base + '&size=' + page_size.to_s + '&offset=' + offset.to_s).tap{ |u| puts "Opening #{u}" }
+  File.open('trainings.xml', 'w') do |f|
+    f.write('<?xml version="1.0" encoding="UTF-8"?><trainings>')
 
-    doc = Nokogiri::XML(open(url))
-    total = doc.css('meta results').text.to_i
+    begin
+      url = "#{base}&size=#{page_size}&offset=#{offset}"
+      puts "Opening #{url}"
+      doc = Nokogiri::XML(open(url))
+      total = doc.css('meta results').text.to_i
 
-    File.open('trainings.xml', 'w') { |f| f.write('<?xml version="1.0" encoding="UTF-8"?><trainings>') } if offset == 0
+      doc.css('training').each do |training|
+        f.write(training.to_s << "\n")
+      end
 
-    doc.css('training').each do |training|
-      File.open('trainings.xml', 'a') {|f| f.write(training.to_s << "\n") }
-    end
+      offset += page_size
+    end while offset < total
 
-    new_offset = (offset + page_size)
-    File.open('trainings.xml', 'w') { |f| f.write('</trainings>') } if new_offset >= total
-
-    # Find the next link and open it, if it exists
-    if(new_offset < total)
-      get_trainings(base, page_size, new_offset)
-    end
-    puts 'Finished crawling'
+    f.write('</trainings>')
+  end
+  puts 'Finished crawling. Data written to trainings.xml'
 end
 
-get_trainings('http://data.springest.nl/trainings.xml?api_key=YOUR_API_KEY')
+get_trainings 'http://data.springest.nl/trainings/search.xml?query=testen&api_key=YOUR_API_KEY&fields=id'
